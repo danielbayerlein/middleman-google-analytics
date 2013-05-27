7096cea609c504776a466eb91d64b5124fb2bcfd
David W. Keith <dwk@nestlabs.com>
2013-05-27 15:37:06 -0700

    MrJoy/middleman-google-analytics#4 Use GA Debug in preview server

--- lib/middleman-google-analytics/extension.rb
+++ lib/middleman-google-analytics/extension.rb
@@ -19,6 +19,7 @@ module Middleman
     module InstanceMethods
       def google_analytics_tag
         options = ::Middleman::GoogleAnalytics.options
+        ga = build? ? 'ga' : '/u/ga_debug'
         if tracking_id = options.tracking_id
           gaq = []
           gaq << ['_setAccount', "#{tracking_id}"]
@@ -30,7 +31,7 @@ module Middleman
   #{gaq.map! { |x| "_gaq.push(#{x});" }.join("\n  ")}
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/#{ga}.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
 //]]>
