42c4fc229baf1558aa5b191fabe7c2cdeb3515f7
David W. Keith <dwk@nestlabs.com>
2013-05-27 16:15:01 -0700

    Remove protocol from URLs
    A byte saved is a byte earned

--- lib/middleman-google-analytics/extension.rb
+++ lib/middleman-google-analytics/extension.rb
@@ -32,7 +32,7 @@ module Middleman
   #{gaq.map! { |x| "_gaq.push(#{x});" }.join("\n  ")}
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/#{ga}.js';
+    ga.src = ('https:' == document.location.protocol ? '//ssl' : '//www') + '.google-analytics.com/#{ga}.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
 //]]>
